a73380c9f7dcd50615225c77147a0e9f62286cf8
ALADDAM Besher <beshoux@outlook.fr>
2021-12-16 14:40:08 +0100

    Les premiers lignes pour le jeu.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
+// Utilisation des bibliotheques necessaires.
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+use macroquad_tiled as tiled;
+
+#[macroquad::main("Platformer")]
 async fn main() {
+
+    /*
+       rotation: f32
+       Rotation in degrees
+
+       zoom: Vec2
+       Scaling, should be (1.0, 1.0) by default
+
+       target: Vec2
+       Rotation and zoom origin
+
+       offset: Vec2
+       Displacement from target
+
+       render_target: Option<RenderTarget>
+       If “render_target” is set - camera will render to texture otherwise to the screen
+
+       viewport: Option<(i32, i32, i32, i32)>
+
+       Part of the screen to render to
+
+       None means the whole screen
+
+       Viewport do not affect camera space, just the render position on the screen
+
+       Usefull for things like splitscreen
+       */
+    let camera = Camera2D::from_display_rect(Rect::new(0.0, 0.0, 320.0, 152.0));
+
     loop {
-        clear_background(RED);
-
-        //        x1    y1    x2     y2     thick color.
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-
-        //[x, y] with size [w, h].
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-
-        //[x, y] with a given radius r.
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-        //[x1, y1] and [x2, y2] thickness color.
-        draw_text("IT WORKS!", 29.0, 20.0, 30.0, DARKGRAY);
-
+        clear_background(WHITE);
+        
+        //Choisir la caméra actif.
+        set_camera(&camera);
+        
         next_frame().await
     }
 }
