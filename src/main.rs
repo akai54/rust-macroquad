de1ff4a865f038556a70384c8c6130eb647aaa2f
ALADDAM Besher <beshoux@outlook.fr>
2021-12-16 14:43:18 +0100

    Commentaire Camera2D.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,10 @@ use macroquad_tiled as tiled;
 #[macroquad::main("Platformer")]
 async fn main() {
 
-    /*
-       rotation: f32
+    /* Explications Camera2D. (Par Ordre).
        Rotation in degrees
 
        zoom: Vec2
-       Scaling, should be (1.0, 1.0) by default
 
        target: Vec2
        Rotation and zoom origin
@@ -23,14 +21,6 @@ async fn main() {
        If “render_target” is set - camera will render to texture otherwise to the screen
 
        viewport: Option<(i32, i32, i32, i32)>
-
-       Part of the screen to render to
-
-       None means the whole screen
-
-       Viewport do not affect camera space, just the render position on the screen
-
-       Usefull for things like splitscreen
        */
     let camera = Camera2D::from_display_rect(Rect::new(0.0, 0.0, 320.0, 152.0));
 
