49bb77e6c319e0d773dbc73e0344bd74b8fc0f9c
ALADDAM Besher <beshoux@outlook.fr>
2021-12-23 23:53:09 +0100

    Ajout tilemap pas fonc encore.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,64 @@
 // Utilisation des bibliotheques necessaires.
 use macroquad::prelude::*;
 
-// use macroquad_tiled as tiled;
+use macroquad_tiled as tiled;
 
 #[macroquad::main("Platformer")]
 async fn main() {
 
     /* Explications Camera2D. (Par Ordre).
        Rotation in degrees
-
        zoom: Vec2
-
        target: Vec2
        Rotation and zoom origin
-
        offset: Vec2
        Displacement from target
-
        render_target: Option<RenderTarget>
        If “render_target” is set - camera will render to texture otherwise to the screen
-
        viewport: Option<(i32, i32, i32, i32)>
        */
     let camera = Camera2D::from_display_rect(Rect::new(0.0, 0.0, 320.0, 152.0));
-    
+
     //Ajout tileset
-    let tileset = load_texture("../Tilemap/spritesheet_jumper.png").await;
+    let tileset = Texture2D::from_file_with_format(
+        include_bytes!("../Tilemap/tileset.png"),
+        None,
+    );
+
+    let portal = Texture2D::from_file_with_format(
+        include_bytes!("../GFX/Items/portal_yellow.png"),
+        None,
+    );
+
+    //Charger le fichier json de la map.
+    let tiled_map_json = load_string("../Tilemap/map.json").await.unwrap();
+
+    let tiled_map = tiled::load_map(
+        &tiled_map_json,
+        &[ 
+        ("tileset.png", tileset), 
+        ("portal_yellow.png", portal),],
+        &[],
+    );
 
     //Ajout texture Personnage (120 x 201).
-    let bunny = Texture2D::from_file_with_format(
-    include_bytes!("../GFX/Players/bunny1_ready.png"), 
-    None,
-    );
-    
-    //Ajout texture ennemie (142 x 148).
-    let ennemie = Texture2D::from_file_with_format(
-    include_bytes!("../images/sun1.png"),
-    None,
+    let _bunny = Texture2D::from_file_with_format(
+        include_bytes!("../GFX/Players/bunny1_ready.png"), 
+        None,
     );
 
-    let width = 700;
-    let height = 500;
+    //Ajout texture ennemie (142 x 148).
+    let ennemie = Texture2D::from_file_with_format(
+        include_bytes!("../images/sun1.png"),
+        None,
+    );
+
+    let _width = 700;
+    let _height = 500;
 
     loop {
         clear_background(WHITE);
-        
+
         //Choisir la caméra actif.
         set_camera(&camera);
 
@@ -54,9 +68,9 @@ async fn main() {
             0.0, 
             WHITE,
             DrawTextureParams {
-            source: Some(Rect::new(0.0, 0.0, 142., 148.)),
-            ..Default::default()
-        },
+                source: Some(Rect::new(0.0, 0.0, 142., 148.)),
+                ..Default::default()
+            },
         );
         next_frame().await
     }
