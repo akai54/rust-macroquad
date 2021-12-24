fa2ec70244fc090f09e93f7b622ad2ce2f9a23ac
ALADDAM Besher <beshoux@outlook.fr>
2021-12-24 16:46:37 +0100

    Fix camera2D.

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,10 @@ async fn main() {
        If “render_target” is set - camera will render to texture otherwise to the screen
        viewport: Option<(i32, i32, i32, i32)>
        */
-    let camera = Camera2D::from_display_rect(Rect::new(0.0, 0.0, 320.0, 152.0));
+    let camera = Camera2D::from_display_rect(Rect::new(0.0, 0.0, screen_width(), screen_height()));
 
-    let width = screen_width();
-    let height = screen_height();
+    let width = 700.;
+    let height = 500.;
 
     //Ajout tileset
     let tileset = Texture2D::from_file_with_format(
@@ -28,10 +28,14 @@ async fn main() {
         None,
     );
 
+    //Sets the FilterMode of this texture.
+    tileset.set_filter(FilterMode::Nearest);
+    
     let decorations = Texture2D::from_file_with_format(
         include_bytes!("../GFX/fishgame_assets/decorations1.png"),
         None,
     );
+    decorations.set_filter(FilterMode::Nearest);
 
     //Charger le fichier json de la map.
     let tiled_map_json = load_string("GFX/fishgame_assets/map.json").await.unwrap();
@@ -45,20 +49,26 @@ async fn main() {
     ).unwrap();
 
     //Ajout texture Personnage (120 x 201).
-    let _bunny = Texture2D::from_file_with_format(
+    let bunny = Texture2D::from_file_with_format(
         include_bytes!("../GFX/Players/bunny1_ready.png"), 
         None,
     );
 
+    bunny.set_filter(FilterMode::Nearest);
+    
     //Ajout texture ennemie (90 x 155).
     let ennemie = Texture2D::from_file_with_format(
         include_bytes!("../GFX/Enemies/spikeMan_stand.png"),
         None,
     );
 
+    ennemie.set_filter(FilterMode::Nearest);
+
     let _ennemie2 = Texture2D::from_file_with_format(include_bytes!("../GFX/Enemies/sun1.png"),None,);
 
     loop {
+        clear_background(BLACK);
+
         //Choisir la caméra actif.
         set_camera(&camera);
 
